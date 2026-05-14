eaa08b253519b91a07dd7731197889eeb668dcea
Guido Giglioli <guido.tobugroup@gmail.com>
2026-05-14 14:28:22 +0200

    consegna (formattata con prettier) 02

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-for (let i=0; i<100; i++) {
-    if (i%3 == 0) {
-        if (i%5 == 0) {
+for (let i = 0; i < 100; i++) {
+    if (i % 3 == 0) {
+        if (i % 5 == 0) {
             console.log("FizzBuzz");
             continue;
         }
         console.log("Fizz");
         continue;
-    } else if (i%5 == 0) {
+    } else if (i % 5 == 0) {
         console.log("Buzz");
         continue;
     } else {
-        console.log(i);        
+        console.log(i);
     }
 }
